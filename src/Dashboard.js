e4e657950e5fdd3079de0a942f0213317393d094
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-03-08 19:27:43 +0530

    fix: listnode integration

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,25 +11,44 @@ import {
     Flex,
     Avatar,
     useDisclosure,
+    AvatarBadge
 } from '@chakra-ui/react';
 import NoteModal from './components/NoteModel';
-import { Client, Databases } from "appwrite";
+import { Link as RouterLink ,useNavigate} from 'react-router-dom';
+import { Client, Databases ,ID , Account} from "appwrite";
+
 
 function Dashboard() {
+    const navigate = useNavigate();
     const [notes, setNotes] = useState();
     const [selectedNote, setSelectedNote] = useState(null);
+    const [getUser , setUser] = useState()
     const { isOpen: isAddModalOpen, onOpen: onAddModalOpen, onClose: onAddModalClose } = useDisclosure();
     const { isOpen: isEditModalOpen, onOpen: onEditModalOpen, onClose: onEditModalClose } = useDisclosure();
 
     const client = new Client();
-
+    const account = new Account(client);
     client
         .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
         .setProject('65c4c6ac2b9a06cef41b'); // Your project ID
 
     const databases = new Databases(client);
 
-    useEffect(() => {
+useEffect(()=>{
+    const getUser = account.get()
+
+    getUser.then(
+        function(response){
+       console.log(response,"get data")
+       setUser(response)
+        },
+        function(error){
+            console.log(error,"get data error")
+        }
+    )
+},[])
+
+    const getData = () => {
         let promise = databases.listDocuments(
             "65c4c8b653802bc0e25f",
             "65c4ca569bf871e35bbb"
@@ -37,36 +56,117 @@ function Dashboard() {
 
         promise.then(
             function (response) {
-                console.log(response, "--------------------");
                 setNotes(response);
+                setSelectedNote(response.documents[0])
             },
             function (error) {
                 console.log(error, "error");
             }
         );
+    }
+
+    useEffect(() => {
+        getData()
+        console.log(notes,"notes")
     }, []);
 
     const user = {
-        name: 'John Doe',
         profilePicture: 'https://bit.ly/dan-abramov',
     };
 
     const handleLogout = () => {
-        console.log('Logged out');
+        let promise = account.deleteSession("current")
+        promise.then(
+            function (response) {
+                if(response){
+                    navigate('/');
+                }
+                
+            },
+            function (error) {
+                console.log(error, "error");
+            }
+        );
     };
 
     const handleViewNote = (note) => {
         setSelectedNote(note);
     };
 
-    const handleEditNote = (note) => {
-        setSelectedNote(note);
-        onEditModalOpen();
+    const handleEditNote = (values) => {
+        console.log(values,"valuesvaluesvalues")
+        let promise = databases.updateDocument(
+            "65c4c8b653802bc0e25f",
+            "65c4ca569bf871e35bbb",
+            values.id,
+             { "Tag" :"tag", 
+            }
+         
+          );
+          promise.then(
+            function (response) {
+                if(response){
+                    getData()
+                    onEditModalClose()
+                }
+                
+            },
+            function (error) {
+                console.log(error, "error");
+            }
+        );
+        
+        // setSelectedNote(note);
+        // onEditModalOpen();
     };
 
-    const addNewNote = () => {
-     console.log("....")
+    const handleAddNewNote = (values) => {
+     let promise = databases.createDocument(
+        "65c4c8b653802bc0e25f",
+        "65c4ca569bf871e35bbb",
+         ID.unique(),
+         { "Tag" :values.tags, 
+         "Note_title" : values.title , 
+        "Note_detail" : values.detail}
+     
+      );
+
+    promise.then(
+        function (response) {
+            if(response){
+                getData()
+                onAddModalClose()
+               
+            }
+            
+        },
+        function (error) {
+            console.log(error, "error");
+        }
+    );
+    
     };
+
+    const handleDeleteNote = (id) =>{ 
+        console.log(id,"id----")
+        let promise = databases.deleteDocument(
+            "65c4c8b653802bc0e25f",
+            "65c4ca569bf871e35bbb",
+            id
+          );
+
+        promise.then(
+            function (response) {
+                if(response){
+                    getData()
+                }
+            },
+            function (error) {
+                console.log(error, "error");
+            }
+        );
+    }
+
 
 
     return (
@@ -75,10 +175,17 @@ function Dashboard() {
                 <Box p={4} bg="purple.600" color="white" w="100%" mb={4}>
                     <Flex justify="end" alignItems="center">
                         <Box textAlign="right" mr={4}>
-                            <Heading size="md" fontWeight="semibold">{user.name}</Heading>
+                            <Heading size="md" fontWeight="semibold">{getUser ? getUser.name : "--"}</Heading>
                             <Button colorScheme="white" onClick={handleLogout} size="sm">Logout</Button>
                         </Box>
-                        <Avatar size="md" src={user.profilePicture} />
+                        {getUser.name &&   <Avatar
+                            bg="#010002"
+                            name={getUser.name}
+                            color="contrast.200"
+                        >
+                            <AvatarBadge boxSize={4} bg="green.500" />
+                        </Avatar>}
+                      
                     </Flex>
                 </Box>
                 <Flex mt={4} w="98%" m={4}>
@@ -107,9 +214,9 @@ function Dashboard() {
                                 <Heading size="md" textAlign="start" mb={2}>{note.Note_title}</Heading>
                                 <Text mb={2} textAlign="start">{note.Note_detail}</Text>
                                 <Stack direction="row" mb={2}>
-                                    {note?.Tag.map((tag, index) => (
-                                        <Badge key={index} colorScheme="green" mr={2}>{tag}</Badge>
-                                    ))}
+                                    {/* {note?.Tag.map((tag, index) => ( */}
+                                        <Badge  colorScheme="green" mr={2}>{note.Tag}</Badge>
+                                    {/* ))} */}
                                 </Stack>
                             </Box>
                         ))}
@@ -121,14 +228,14 @@ function Dashboard() {
                                 <Heading size="md" textAlign="start" mb={2}>{selectedNote.Note_title}</Heading>
                                 <Text textAlign="start" mb={4}>{selectedNote.Note_detail}</Text>
                                 <Stack direction="row" mb={2}>
-                                    {selectedNote?.Tag.map((tag, index) => (
-                                        <Badge key={index} colorScheme="green" mr={2}>{tag}</Badge>
-                                    ))}
+                                    {/* {selectedNote?.Tag.map((tag, index) => ( */}
+                                        <Badge colorScheme="green" mr={2}>{selectedNote.Tag}</Badge>
+                                    {/* ))} */}
                                 </Stack>
                                 <Divider mt={2} mb={2} />
                                 <Stack direction="row" justify="flex-end">
                                     <Button colorScheme="blue" size="sm" onClick={onEditModalOpen}>Edit</Button>
-                                    <Button colorScheme="red" size="sm" ml={2}>Delete</Button>
+                                    <Button colorScheme="red" size="sm" ml={2} onClick={()=>{handleDeleteNote(selectedNote.$id)}} >Delete</Button>
                                 </Stack>
                             </Box>
                         )}
@@ -138,20 +245,20 @@ function Dashboard() {
                 <NoteModal
                     isOpen={isAddModalOpen}
                     onClose={onAddModalClose}
-                    onSave={addNewNote} // Placeholder function
-                    title=""
-                    detail=""
-                    tags={[]}
+                    onSave={(values)=>{
+                        handleAddNewNote(values)
+                    }} 
+                  
                 />
                 {/* Edit Note Modal */}
                 {selectedNote && (
                     <NoteModal
                         isOpen={isEditModalOpen}
                         onClose={onEditModalClose}
-                        onSave={() => {}} // Placeholder function
-                        title={selectedNote.Note_title} // Use selectedNote.Note_title instead of selectedNote.title
-                        detail={selectedNote.Note_detail} // Use selectedNote.Note_detail instead of selectedNote.detail
-                        tags={selectedNote.Tag} // Use selectedNote.Tag instead of selectedNote.tags
+                        onSave={(values)=>{
+                            handleEditNote(values)
+                    }} 
+                    data={selectedNote}
                     />
                 )}
             </Flex>
--- src/Login.js
+++ src/Login.js
@@ -1,36 +1,72 @@
 import React from 'react';
-import { ChakraProvider, Box, FormControl, FormLabel, Input, Button, Heading, Link } from '@chakra-ui/react';
+import { ChakraProvider, Box, Heading, FormControl, FormLabel, Input, Button, Link } from '@chakra-ui/react';
+import { Formik, Form, Field } from "formik"
 import { Link as RouterLink ,useNavigate} from 'react-router-dom';
+import { Client, Databases , ID ,Account } from "appwrite";
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const client = new Client();
+
+  const account = new Account(client);
+
+  client
+  .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+  .setProject('65c4c6ac2b9a06cef41b') // Your project ID
+;
+
+  const logIn = (values) =>{
+    const promise = account.createEmailSession(values.email,values.password);
+
+    promise.then(function (response) {
+        // account.createEmailSession(values.email,values.password)
+        navigate('/dashboard');
+    }, function (error) {
+    });
+  }
   return (
     <ChakraProvider>
-      <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg" margin="auto" mt={20}>
-        <Box textAlign="center">
-          <Heading>Sign In</Heading>
-        </Box>
-        <Box my={4}>
-          <form>
-            <FormControl>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" placeholder="Enter email" />
-            </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder="Enter password" />
-            </FormControl>
-            <Button width="full" mt={4} colorScheme="purple" type="submit" onClick={()=>{   navigate('/dashboard');}}>
+    <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg" margin="auto" mt={20}>
+      <Box textAlign="center">
+        <Heading>Sign In</Heading>
+      </Box>
+      <Box my={4}>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            logIn(values); 
+          }}
+        >
+          <Form>
+            <Field name="email">
+              {({ field }) => (
+                <FormControl>
+                  <FormLabel>Email address</FormLabel>
+                  <Input {...field} type="email" placeholder="Enter email" />
+                </FormControl>
+              )}
+            </Field>
+            <Field name="password">
+              {({ field }) => (
+                <FormControl mt={4}>
+                  <FormLabel>Password</FormLabel>
+                  <Input {...field} type="password" placeholder="Enter password" />
+                </FormControl>
+              )}
+            </Field>
+            <Button width="full" mt={4} colorScheme="purple" type="submit">
               Sign In
             </Button>
-          </form>
-          <Box mt={2} textAlign="center">
-            <Link as={RouterLink} to="/signUp" color="purple.500">
-              Need an account? Sign up
-            </Link>
-          </Box>
+          </Form>
+        </Formik>
+        <Box mt={2} textAlign="center">
+          <Link href="/signUp" color="purple.500">
+            Need an account? Sign up
+          </Link>
         </Box>
       </Box>
-    </ChakraProvider>
+    </Box>
+  </ChakraProvider>
   );
 }
